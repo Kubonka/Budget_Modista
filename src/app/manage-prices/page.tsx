a3e0901aae9a513d24293b1ed51c5872f309969f
Kubonka <srbetito85@gmail.com>
2024-01-04 08:06:41 -0300

    styles prices

--- src/app/manage-prices/page.tsx
+++ src/app/manage-prices/page.tsx
@@ -95,7 +95,7 @@ function ManagePrices() {
 																	{`${capitalize(subcategory.name)} `}
 																</p>
 																<div className="flex items-center gap-1">
-																	<p className="text-right border-[1px] border-red-200 w-[40px] text-[16px] font-semibold text-title-strong flex-nowrap">
+																	<p className="text-right border-[1px] border-red-200 w-[50px] text-[16px] font-semibold text-title-strong flex-nowrap">
 																		{"$" + priceFound?.value}
 																	</p>
 																	<PriceDialogUpdate
